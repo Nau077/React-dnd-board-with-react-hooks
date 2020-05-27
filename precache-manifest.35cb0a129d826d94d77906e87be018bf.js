b83efbfa63cb2396ad3f1519d62c6aa41edd6f3e
Nau077 <antbobrikov@yandex.ru>
2020-05-28 00:21:48 +0300

    Updates

---
+++ precache-manifest.35cb0a129d826d94d77906e87be018bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357a2dd0263ea06ccd06999c6eb9154e",
+    "url": "/React-dnd-board-with-react-hooks/index.html"
+  },
+  {
+    "revision": "deecec6f95a09b5df25f",
+    "url": "/React-dnd-board-with-react-hooks/static/css/main.b44f8e1d.chunk.css"
+  },
+  {
+    "revision": "5147c3bee00bbe221aed",
+    "url": "/React-dnd-board-with-react-hooks/static/js/2.99f3a09a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-dnd-board-with-react-hooks/static/js/2.99f3a09a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deecec6f95a09b5df25f",
+    "url": "/React-dnd-board-with-react-hooks/static/js/main.64e87005.chunk.js"
+  },
+  {
+    "revision": "b9da607f447346817754",
+    "url": "/React-dnd-board-with-react-hooks/static/js/runtime-main.f21f8f49.js"
+  }
+]);
